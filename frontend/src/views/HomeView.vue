c460441cc141da1b210fe36b27576e5cabe1c33d
Roman Zhdanov <zhdanov.roma@gmail.com>
2023-06-01 14:45:56 +0300

    Created pagination component and added it to home.

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,31 +1,52 @@
 <template>
   <main>
-    <ul v-if="games">
-      <li v-for="game in games" :key="game.id">
-        {{ game.title }}
-      </li>
-    </ul>
+    <div v-if="isLoading">Loading...</div>
+    <div v-else-if="pageResult">
+      <ul>
+        <li v-for="game in pageResult.items" :key="game.id">
+          {{ game.title }}
+        </li>
+      </ul>
+      <base-pagination
+        :currentPage="page"
+        :totalPages="pageResult.totalPages"
+        :hasNext="pageResult.hasNextPage"
+        :hasPrevious="pageResult.hasPreviousPage"
+        @page-changed="selectPage"
+      />
+    </div>
   </main>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
 import { gamesService } from '@/services/'
-import type { GameItem } from '@/types/gameTypes'
+import type { GamesPageResult } from '@/types/gameTypes'
+import BasePagination from '@/components/ui/BasePagination.vue'
 
 export default defineComponent({
+  components: {
+    BasePagination
+  },
   data() {
     return {
-      games: {} as GameItem[],
-      page: 1,
-      pageSize: 10
+      pageResult: {} as GamesPageResult,
+      page: 1 as number,
+      pageSize: 10,
+      isLoading: false
     }
   },
   methods: {
     async loadGamesPage(): Promise<void> {
+      this.isLoading = true
       const { data, status } = await gamesService.getPage(this.page, this.pageSize)
-      this.games = data.items
+      this.pageResult = data
+      this.isLoading = false
       console.log('response status is: ', status)
+    },
+    selectPage(page: number) {
+      this.page = page
+      this.loadGamesPage()
     }
   },
   created() {
